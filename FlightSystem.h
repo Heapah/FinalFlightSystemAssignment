562d62b325f9e07418ccd24be178a4830a5d30ac
Heapah <33457316+Heapah@users.noreply.github.com>
2017-11-14 15:15:02 +0000

    FlightSystem.h

---
+++ FlightSystem.h
@@ -0,0 +1,28 @@
+// Name: Ethan Heap
+// Student Number: B00714306
+// Date: 14/11/17
+// Subject: OOP - Flight Control System Assignment
+
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+#include"Aircraft.h"
+
+class FlightSystem {
+
+	// Setting the FlightSystem variables
+
+private:
+
+	aircraftList_ : std::vector<Aircraft>
+
+public:
+
+	AddAircraft(std::string flightNumber, std::string airline, std::string aircraftType,
+		std::string gridReference, int groundSpeed, int altitude, int heading);
+
+	
+
+
+};
