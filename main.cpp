e8a2ec11d9818394e3032d24dd70a5af007979f8
Heapah <33457316+Heapah@users.noreply.github.com>
2017-11-14 15:44:59 +0000

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//Ethan to put header info in here
+#pragma once
+
+#include <iostream>
+#include "Aircraft.h"
+
+using namespace std;
+
+int main() {
+
+	//Create some aircraft objects and test them
+
+	Aircraft a1{};
+	a1.SetAircraftType("A380");
+	cout << "Hello world" << endl;
+
+
+	return 0;
+
+
+}
