ca91b0b697d7b93879d8370dec4ff867c9e4964e
Heapah <33457316+Heapah@users.noreply.github.com>
2017-11-14 22:19:21 +0000

    FlightSystem.h

--- FlightSystem.h
+++ FlightSystem.h
@@ -15,14 +15,20 @@ class FlightSystem {
 
 private:
 
-	aircraftList_ : std::vector<Aircraft>
+	std::vector<Aircraft> aircraftList_;
 
 public:
+	FlightSystem();
 
-	AddAircraft(std::string flightNumber, std::string airline, std::string aircraftType,
+	void AddAircraft(std::string flightNumber, std::string airline, std::string aircraftType,
 		std::string gridReference, int groundSpeed, int altitude, int heading);
-
-	
-
+	std::vector<Aircraft> ListAllAircraft();
+	std::vector<Aircraft> ListAllCruisingAircraft();
+	void RemoveAircraft(std::string flightNumber);
+	void ChangeAltitude(std::string flightNumber, int altitude);
+	int GetAltitude(std::string flightNumber);
+	void ChangeHeading(std::string flightNumber, int heading);
+	int GetHeading(std::string flightNumber);
+	int numAircraftInSector();
 
 };
