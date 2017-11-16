01ea3515742bad3f4a4b32ce414ea1e6cd9f8e77
Heapah <33457316+Heapah@users.noreply.github.com>
2017-11-16 12:55:32 +0000

    main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main() {
 
 	FlightSystem a1;
-	
+
 	std::string flightNumber;
 	std::string airline;
 	std::string aircraftType;
@@ -26,6 +26,7 @@ int main() {
 		cout << "[Flight Control System Main Menu]" << endl;
 		cout << "[Please press the specified key to choose that option]" << endl;
 		cout << "[1.) Add an Aircraft to the system]" << endl;
+		cout << "[2.) Print a List of all Aircrafts]" << endl;
 
 		std::cin >> useroption;
 		switch (useroption) {
@@ -51,11 +52,82 @@ int main() {
 			a1.AddAircraft(flightNumber, airline, aircraftType,
 				gridReference, groundSpeed, altitude, heading);
 		}
+		break;
+
+		case 2:
+		{
+			cout << "[List of all Aircrafts]" << endl;
+			cout << "[Flight # | Airline | Aircraft Type | Grid Reference | Ground Speed | Altitude | Heading]" << endl;
+			vector<Aircraft> AircraftList = FlightSystem.ListAllAircraft();
+			for (int i = 0; AircraftList.size(); i++) {
+				// Printing the Flight Number
+				flightNumber = AircraftList[i].GetFlightNumber();
+				cout << flightNumber;
+
+				// Printing the Airline
+				airline = AircraftList[i].GetAirline();
+				cout << airline;
+
+				// Printing the Aircraft Type
+				aircraftType = AircraftList[i].GetAircraftType();
+				cout << aircraftType;
+
+				// Printing the Grid Reference
+				gridReference = AircraftList[i].GetGridReference();
+				cout << gridReference;
+
+				// Printing the Ground Speed
+				groundSpeed = AircraftList[i].GetGroundSpeed();
+				cout << groundSpeed;
+
+				// Printing the Altitude
+				altitude = AircraftList[i].GetAltitude();
+				cout << altitude;
+
+				// Printing the Heading
+				heading = AircraftList[i].GetHeading();
+				cout << heading << endl;
+			}
+			break;
 		}
+		case 3:
+			cout << "[List of all Aircrafts above 30000 feet]" << endl;
+			cout << "[Flight # | Airline | Aircraft Type | Grid Reference | Ground Speed | Altitude | Heading]" << endl;
+			vector<Aircraft> AircraftListAbove30k = FlightSystem.ListAllCruisingAircraft();
+			for (int i = 0; AircraftListAbove30k.size(); i++) {
+				// Printing the Flight Number
+				flightNumber = AircraftListAbove30k[i].GetFlightNumber();
+				cout << flightNumber;
+
+				// Printing the Airline
+				airline = AircraftListAbove30k[i].GetAirline();
+				cout << airline;
+
+				// Printing the Aircraft Type
+				aircraftType = AircraftListAbove30k[i].GetAircraftType();
+				cout << aircraftType;
+
+				// Printing the Grid Reference
+				gridReference = AircraftListAbove30k[i].GetGridReference();
+				cout << gridReference;
+
+				// Printing the Ground Speed
+				groundSpeed = AircraftListAbove30k[i].GetGroundSpeed();
+				cout << groundSpeed;
+
+				// Printing the Altitude
+				altitude = AircraftListAbove30k[i].GetAltitude();
+				cout << altitude;
+
+				// Printing the Heading
+				heading = AircraftListAbove30k[i].GetHeading();
+				cout << heading << endl;
+			}
+			break;
+		}
+
+
 	}
-
-
-
 	while (useroption != 0);
 
-} 
+}
